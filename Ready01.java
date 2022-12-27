792281fccc6427ee997099487fc3e92a34050890
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-12-27 02:44:40 +0000

    [Add]コメント追加

--- Ready01.java
+++ Ready01.java
@@ -1,5 +1,11 @@
+// クラスブロックの始まり
 public class Ready01 {
+    メソッドブロックの始まり（インデントして分かりやすくする）
     public static void main (String[] args) {
-        System.out.println("Hellow World!");
+        コメントを追加（メソッドブロックよりさらにインデントして分かりやすくする）
+        // メソッド処理を記述（今回は、文字列を出力する処理）
+        System.out.println("Hello World!");
+    // メソッドの終わり
     }
+// クラスメソッドの終わり
 }
