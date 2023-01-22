503e0a9f96350b3e37299f088a7e0666826c2665
EC2 Default User <sk2361ksg@gmail.com>
2023-01-22 10:55:10 +0000

    [Start]javaカリキュラムを初めから開始

--- Ready01.java
+++ Ready01.java
@@ -1,11 +1,5 @@
-// クラスブロックの始まり
 public class Ready01 {
-    メソッドブロックの始まり（インデントして分かりやすくする）
     public static void main (String[] args) {
-        コメントを追加（メソッドブロックよりさらにインデントして分かりやすくする）
-        // メソッド処理を記述（今回は、文字列を出力する処理）
         System.out.println("Hello World!");
-    // メソッドの終わり
     }
-// クラスメソッドの終わり
 }
