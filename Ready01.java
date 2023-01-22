52fe488bcd3202a05e789d1bd8a428f96ce04041
EC2 Default User <sk2361ksg@gmail.com>
2023-01-22 11:42:35 +0000

    [Add]出力練習

--- Ready01.java
+++ Ready01.java
@@ -1,5 +1,6 @@
-public class Ready01 {
-    public static void main (String[] args) {
-        System.out.println("Hello World!");
-    }
+
+public class Chapter02 {
+	public static void main(String[] args) {
+		System.out.println("こんにちは!");
+	}
 }
