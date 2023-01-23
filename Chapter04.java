0b968f97eb90ca6e11949d286a61d4e5821caff7
EC2 Default User <sk2361ksg@gmail.com>
2023-01-23 02:35:51 +0000

    [Add]変数・定数

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "so";
+		System.out.println(name);
+		name = "muto";
+		System.out.println(name);
+		final int number = 7;
+		System.out.println(number);
+	}
+}
