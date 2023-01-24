4637a8d6c9818d4255b200df452f68851d7e9531
EC2 Default User <sk2361ksg@gmail.com>
2023-01-24 06:24:21 +0000

    [Add]if文

---
+++ if01.java
@@ -0,0 +1,15 @@
+
+public class if01 {
+	public static void main(String[] args) {
+		int number=7;
+		if(number<5) {
+			System.out.println(number+"は５未満です");
+		}
+		if(number<10) {
+			System.out.println(number+"は１０未満です");
+		}
+		if(number<15) {
+			System.out.println(number+"は１５未満です");
+		}
+	}
+}
