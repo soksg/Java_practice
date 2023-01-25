9a2932fdfd736eeb53e3394f03ecdd6e2d8d94d2
EC2 Default User <sk2361ksg@gmail.com>
2023-01-25 02:34:09 +0000

    [Add]条件分岐

---
+++ Chapter07.java
@@ -0,0 +1,15 @@
+
+public class Chjapter7 {
+	public static void main(String[] args) {
+		int distance=15;
+		if (distance<=5) {
+			System.out.println("とても近いです");
+		}else if(distance<5){
+			System.out.println("近いです");
+		}else if(distance<10){
+			System.out.println("遠いです");
+		}else{
+			System.out.println("とても遠いです");
+		}
+	}
+}
