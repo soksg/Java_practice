811c3543e66b2d46897e68db8644ae5ee8ee5a19
EC2 Default User <sk2361ksg@gmail.com>
2023-02-15 01:52:51 +0000

    [Add]定義復習

--- Variable01.java
+++ Variable01.java
@@ -14,3 +14,28 @@ public class Variable01 {
 		System.out.println(num2);
 	}
 }
+
+
+public class Variable01 {
+	public static void main(String[] args) {
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World";
+		System.out.println(str1);
+		System.out.println(num1);
+
+		num1 = 15;
+		str1 = "Hello Java";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		int num2=20;
+		System.out.println(num2);
+
+		final int FIN_NUM = 30;
+		System.out.println(FIN_NUM);
+
+	}
+}
+
