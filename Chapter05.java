d0966933952460c8327b0d4531c87542f8980af2
EC2 Default User <sk2361ksg@gmail.com>
2023-02-15 03:04:45 +0000

    [Add]型の学習

---
+++ Chapter05.java
@@ -0,0 +1,11 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long_value=10;
+		double double_value=0.1;
+		boolean a = true;
+		System.out.println(long_value);
+		System.out.println(double_value);
+		System.out.println(a);
+	}
+}
