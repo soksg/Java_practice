d1aa0779a5606cd202d7da931b6fda8f21b0e199
EC2 Default User <sk2361ksg@gmail.com>
2023-02-23 10:10:11 +0000

    [Add]問題２二回目

--- Chapter07.java
+++ Chapter07.java
@@ -34,3 +34,25 @@ public class Chjapter7 {
 	}
 }
 
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		String color="blue";
+		switch (color){
+			case "red":
+				System.out.println("赤信号です");
+			break;
+			case "yellow":
+				System.out.println("黄信号です");
+			break;
+			case "blue":
+				System.out.println("青信号です");
+			break;
+			default:
+				System.out.println("信号の色ではありません");
+//			break;
+		}
+	}
+}
+
+
