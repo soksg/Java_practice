19eea4d811a43b05d7ca46a5035a31b235d16ded
EC2 Default User <sk2361ksg@gmail.com>
2023-02-23 10:35:15 +0000

    [Add]１から100madeno

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,14 +1,18 @@
+
 public class FizzBuzz {
 	public static void main(String[] args) {
-		int num = 30;
-		if (num % 15 == 0) {
-			System.out.println("FizzBuzz");
-		} else if (num % 3 == 0) {
-			System.out.println("Fizz");
-		} else if (num % 5 == 0) {
-			System.out.println("Buzz");
-		} else {
-			System.out.println(num);
+		int num = 1;
+		while (num<100){
+			num += 1;
+			if (num % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (num % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (num % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(num);
+			}
 		}
 	}
 }
