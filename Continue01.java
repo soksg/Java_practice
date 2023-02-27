57c6f2e0c5efaf00e94e691d83390279f1e6b479
EC2 Default User <sk2361ksg@gmail.com>
2023-02-27 04:23:22 +0000

    [Add]拡張for文２回目及びcontinue文

---
+++ Continue01.java
@@ -0,0 +1,11 @@
+
+public class Continiue01 {
+	public static void main(String[] args) {
+		for(int count = 0; count < 5; count ++) {
+			if(count == 2) {
+				continue;
+			}
+		System.out.println("Count01 =   " + count);
+		}
+	}
+}
--- For02.java
+++ For02.java
@@ -6,3 +6,13 @@ public class For02 {
 		}
 	}
 }
+
+git
+public class For02 {
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,5};
+		for (int number : array) {
+			System.out.println("For02 = " + number);
+		}
+	}
+}
